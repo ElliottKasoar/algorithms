6761ac554a69db066cb2a223dc81cdca6f988558
Elliott Kasoar <elliott.kasoar@btinternet.com>
2020-04-07 22:22:31 +0100

    Sort first pairs

--- mergesort.py
+++ mergesort.py
@@ -9,7 +9,10 @@ Created on Tue Apr  7 17:55:44 2020
 import numpy as np
 
 ol = np.linspace(0,9,10)
-np.random.shuffle(ol)
+ul = ol.copy() #Otherwise just renames ol as ul and will still be shuffled
+np.random.shuffle(ul)
+print("ol: ", ol)
+print("ul: ", ul)
 
 # Assume even number of elements in list for now...
 # Count length, not sure if will need but useful to know in general...
@@ -17,8 +20,41 @@ np.random.shuffle(ol)
 length = len(ol) #Could use np.size or np.shape[0]
 
 for i in range(length//2):
-    print(i)
+    # print(i)
+    i
 
-print("test")
-print("test 2")
+# Will be ordered lowest to highest
 
+#First set of pairs:
+if (ul[0] < ul[1]):
+    pair_1 = np.array([ul[0], ul[1]])
+else:
+    pair_1 = np.array([ul[1], ul[0]])
+
+if (ul[2] < ul[3]):
+    pair_2 = np.array([ul[2], ul[3]])
+else:
+    pair_2 = np.array([ul[3], ul[2]])
+    
+if (ul[4] < ul[5]):
+    pair_3 = np.array([ul[4], ul[5]])
+else:
+    pair_3 = np.array([ul[5], ul[4]])
+    
+if (ul[6] < ul[7]):
+    pair_4 = np.array([ul[6], ul[7]])
+else:
+    pair_4 = np.array([ul[7], ul[6]])
+
+if (ul[8] < ul[9]):
+    pair_5 = np.array([ul[8], ul[9]])
+else:
+    pair_5 = np.array([ul[9], ul[8]])
+
+print(pair_1)
+print(pair_2)
+print(pair_3)
+print(pair_4)
+print(pair_5)
+
+# Second set of pairs:
