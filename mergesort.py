4fbf23657275448dc93dc22929d152ea8522d8a3
Elliott Kasoar <elliott.kasoar@btinternet.com>
2020-04-07 23:53:53 +0100

    Use temp rather than create new variables for each

--- mergesort.py
+++ mergesort.py
@@ -25,36 +25,77 @@ for i in range(length//2):
 
 # Will be ordered lowest to highest
 
-#First set of pairs:
-if (ul[0] < ul[1]):
-    pair_1 = np.array([ul[0], ul[1]])
-else:
-    pair_1 = np.array([ul[1], ul[0]])
+# =============================================================================
+# First set of pairs:
+# =============================================================================
 
-if (ul[2] < ul[3]):
-    pair_2 = np.array([ul[2], ul[3]])
-else:
-    pair_2 = np.array([ul[3], ul[2]])
+if (ul[0] > ul[1]):
+    temp = ul[1]
+    ul[1] = ul[0]
+    ul[0] = temp
     
-if (ul[4] < ul[5]):
-    pair_3 = np.array([ul[4], ul[5]])
-else:
-    pair_3 = np.array([ul[5], ul[4]])
+if (ul[2] > ul[3]):
+    temp = ul[3]
+    ul[3] = ul[2]
+    ul[2] = temp
     
-if (ul[6] < ul[7]):
-    pair_4 = np.array([ul[6], ul[7]])
-else:
-    pair_4 = np.array([ul[7], ul[6]])
+if (ul[4] > ul[5]):
+    temp = ul[5]
+    ul[5] = ul[4]
+    ul[4] = temp
+    
+if (ul[6] > ul[7]):
+    temp = ul[7]
+    ul[7] = ul[6]
+    ul[6] = temp
+    
+if (ul[8] > ul[9]):
+    temp = ul[9]
+    ul[9] = ul[8]
+    ul[8] = temp
 
-if (ul[8] < ul[9]):
-    pair_5 = np.array([ul[8], ul[9]])
-else:
-    pair_5 = np.array([ul[9], ul[8]])
+print(ul)
 
-print(pair_1)
-print(pair_2)
-print(pair_3)
-print(pair_4)
-print(pair_5)
+# if (ul[0] < ul[1]):
+#     pair_1 = np.array([ul[0], ul[1]])
+# else:
+#     pair_1 = np.array([ul[1], ul[0]])
 
+# if (ul[2] < ul[3]):
+#     pair_2 = np.array([ul[2], ul[3]])
+# else:
+#     pair_2 = np.array([ul[3], ul[2]])
+    
+# if (ul[4] < ul[5]):
+#     pair_3 = np.array([ul[4], ul[5]])
+# else:
+#     pair_3 = np.array([ul[5], ul[4]])
+    
+# if (ul[6] < ul[7]):
+#     pair_4 = np.array([ul[6], ul[7]])
+# else:
+#     pair_4 = np.array([ul[7], ul[6]])
+
+# if (ul[8] < ul[9]):
+#     pair_5 = np.array([ul[8], ul[9]])
+# else:
+#     pair_5 = np.array([ul[9], ul[8]])
+
+# print(pair_1)
+# print(pair_2)
+# print(pair_3)
+# print(pair_4)
+# print(pair_5)
+
+
+# =============================================================================
 # Second set of pairs:
+# =============================================================================
+
+
+
+
+
+
+
+
