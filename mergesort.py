f28aa441bee1f31756c48dd72ff470b2231c2ffe
Elliott Kasoar <elliott.kasoar@btinternet.com>
2020-04-07 18:27:42 +0100

    [First commit]

---
+++ mergesort.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  7 17:55:44 2020
+
+@author: Elliott
+"""
+
+import numpy as np
+
+ol = np.linspace(0,9,10)
+
+# Assume even number of elements in list for now...
+# Count length, not sure if will need but useful to know in general...
+#Should be half length number of pairs
+length = len(ol) #Could use np.size or np.shape[0]
+
+for i in range(length//2):
+    print(i)
